8628a87528d70aea469ae5d82854591d18d97c42
dsdy <dsdy06@gmail.com>
2022-08-29 10:04:21 +0300

    minor fix of user-pic element

---
+++ src/components/ui/user-icon/user-icon.stories.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import UserIcon from "./user-icon"
+
+export default {
+  title: "user-icon",
+  component: UserIcon,
+  argTypes: {
+    size: {
+      options: ["small", "big", "medium"],
+      control: { type: "radio" },
+    },
+  },
+}
+
+function Template(args) {
+  return (
+    <div style={{ backgroundColor: "#ccc" }}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <UserIcon {...args} />
+    </div>
+  )
+}
+
+export const Big = Template.bind({})
+
+Big.args = {
+  size: "big",
+}
+
+export const Small = Template.bind({})
+
+Small.args = {
+  size: "small",
+}
+
+export const Medium = Template.bind({})
+
+Medium.args = {
+  size: "medium",
+}
