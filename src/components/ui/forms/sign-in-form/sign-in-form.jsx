8016e0d63615755aa676c51c23fddaae9d45eac2
dsdy <dsdy06@gmail.com>
2022-09-04 00:58:42 +0300

    added incorrect password message to sign in form

--- src/components/ui/forms/sign-in-form/sign-in-form.jsx
+++ src/components/ui/forms/sign-in-form/sign-in-form.jsx
@@ -1,13 +1,31 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react"
+import propTypes from "prop-types"
 import "../style.css"
 import InputField from "../../input-field/input-field"
 import Button from "../../button/button"
 
-export default function SignIn() {
+const incorrectStyles = ["incorrect-password"]
+export default function SignIn(props) {
+  const { error } = props
+  SignIn.propTypes = {
+    /**
+     * shows wrong passport message
+     */
+    error: propTypes.bool,
+  }
+  SignIn.defaultProps = {
+    error: false,
+  }
+  if (error) {
+    incorrectStyles.push("incorrect-password_visible")
+  }
   return (
     <div className="sign-in">
       <h1>Sign In</h1>
+      <div className={incorrectStyles}>
+        Your password is incorrect. Please,try again
+      </div>
       <form>
         <label>
           Name
