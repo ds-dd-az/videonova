dee9296b35d723409029633759a0800e1068236d
dsdy <dsdy06@gmail.com>
2022-09-05 04:24:46 +0300

    done for now

--- src/components/ui/video/video.jsx
+++ src/components/ui/video/video.jsx
@@ -1,12 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactPlayer from "react-player"
 import propTypes from "prop-types"
 import "./style.css"
 
 export default function Video(props) {
-  const { videoName, videoDescription } = props
-  let { videoUrl } = props
-  videoUrl = "https://www.youtube.com/watch?v=rYNF_541naM"
+  const { videoUrl, videoName, videoDescription, playerVisible } = props
   let videoId = videoUrl.split("v=")[1]
   const ampersandPosition = videoId.indexOf("&")
   if (ampersandPosition !== -1) {
@@ -25,31 +23,65 @@ export default function Video(props) {
      * description
      */
     videoDescription: propTypes.string,
+    /**
+     * visibility of big player
+     */
+    playerVisible: propTypes.bool,
   }
 
   Video.defaultProps = {
     videoUrl: "https://www.youtube.com/watch?v=VSNb6944cE8",
     videoName: "Your great video",
     videoDescription: "very great video, everyone likes it",
+    playerVisible: false,
+  }
+  const [visibility, setVisibility] = useState(false)
+  function toggleVisibility() {
+    setVisibility(!visibility)
+  }
+
+  if (visibility) {
+    return (
+      <div className="video-component">
+        <ReactPlayer
+          className="react-player"
+          url={videoUrl}
+          width="854px"
+          height="480px"
+          config={{
+            youtube: {
+              playerVars: {
+                controls: 1,
+                showinfo: 0,
+                iv_load_policy: 3,
+              },
+            },
+          }}
+        />
+        <div
+          className="thumbnail-wrapper"
+          role="button"
+          tabIndex="0"
+          onKeyDown={toggleVisibility}
+          onClick={toggleVisibility}
+        >
+          <img src={`https://img.youtube.com/vi/${videoId}/0.jpg`} alt="" />
+        </div>
+        <h3>{videoName}</h3>
+        {videoDescription}
+      </div>
+    )
   }
 
   return (
     <div className="video-component">
-      <ReactPlayer
-        className="react-player"
-        url={videoUrl}
-        width="314px"
-        config={{
-          youtube: {
-            playerVars: {
-              controls: 1,
-              showinfo: 0,
-              iv_load_policy: 3,
-            },
-          },
-        }}
-      />
-      <div className="thumbnail-wrapper">
+      <div
+        className="thumbnail-wrapper"
+        role="button"
+        tabIndex="0"
+        onKeyDown={toggleVisibility}
+        onClick={toggleVisibility}
+      >
         <img src={`https://img.youtube.com/vi/${videoId}/0.jpg`} alt="" />
       </div>
       <h3>{videoName}</h3>
