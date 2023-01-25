25fb121c9d35a1afcec357fcd20b6c5329d869d5
dsdyaz <dsdy06@gmail.com>
2023-01-25 23:40:56 +0200

    added check icon, also configured css for it

--- src/components/ui/forms/add-new-video_added/video-added.jsx
+++ src/components/ui/forms/add-new-video_added/video-added.jsx
@@ -2,7 +2,12 @@ import React from "react"
 import "../style.css"
 import propTypes from "prop-types"
 import Button from "../../button/button"
+import CheckIcon from "../../../../assets/icons/check-icon.svg"
 
+const checkIcon = {
+  src: CheckIcon,
+  alt: "done",
+}
 export default function VideoAdded(props) {
   const { img } = props
   VideoAdded.propTypes = {
@@ -13,11 +18,18 @@ export default function VideoAdded(props) {
   }
   return (
     <div className="video-added">
-      <h1>Video succesfully added</h1>
+      <h1>Add New Video</h1>
       <div>
         <div className="vid-preview">
-          <img src={`${img}`} alt="prev" />
+          <div className="vid-preview__shadow" />
+          <img src={`${img}`} className="vid-preview__image" alt="prev" />
+          <img
+            src={checkIcon.src}
+            className="vid-preview__check-icon"
+            alt={checkIcon.alt}
+          />
         </div>
+        <h2>Successful</h2>
         <Button text="To video" />
       </div>
     </div>
