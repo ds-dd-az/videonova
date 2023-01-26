6b4d81292117a37fcadb720ea196cad29a1a9977
dsdyaz <dsdy06@gmail.com>
2023-01-26 16:21:33 +0200

    created sorting by name

--- src/external_func/displaying-users/users.jsx
+++ src/external_func/displaying-users/users.jsx
@@ -3,10 +3,19 @@ import { useSelector } from "react-redux"
 import { SelectUsers, SelectVideos } from "../../modules/userdata"
 import UserCard from "../../components/ui/user-card/user-card"
 
+function sortByName(a, b) {
+  if (a.slug > b.slug) return 1
+  if (a.slug < b.slug) return -1
+  return 0
+}
+
 export default function Users() {
   const users = useSelector(SelectUsers)
   const videos = useSelector(SelectVideos)
-  const usersArr = users.map((element) => {
+  const initialArr = users.map((arr) => arr)
+  console.log(initialArr)
+  console.log(initialArr.sort(sortByName))
+  const usersArr = initialArr.map((element) => {
     const countVideos = videos.filter((value) => value.userId === element.id)
     return (
       <UserCard
