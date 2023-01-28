5c69ce189e0878e4b7babce5a46e140bc4ce2a45
dsdyaz <dsdy06@gmail.com>
2023-01-29 00:33:27 +0200

    refactored users page

--- src/pages/user-page/user-page.jsx
+++ src/pages/user-page/user-page.jsx
@@ -1,44 +1,26 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Layout from "../../components/layout/layout"
-import Video from "../../components/ui/video/video"
 import UserIcon from "../../components/ui/user-icon/user-icon"
 import Button from "../../components/ui/button/button"
 import VidIcon from "./video-icon.png"
 import {
-  SelectVideos,
   SelectUsers,
   SelectLoading,
+  SelectCurrentUserId,
 } from "../../modules/userdata"
 import LoadRing from "../../components/ui/Loading-ring/load-ring"
-
-const vidIcon = {
-  src: VidIcon,
-  alt: "",
-}
+import { addVideoForm } from "../../external_func/dispatches/dispatches"
+import Videos from "../../containers/videos/videos"
 
 export default function UserPage() {
   const dispatch = useDispatch()
   const pageId = window.location.href.split("user/")[1]
-  function showForm() {
-    dispatch({
-      type: "form/show",
-      payload: { type: "addVideo" },
-    })
-  }
+  const openForm = () => addVideoForm(dispatch)
   const users = useSelector(SelectUsers)
+  const currentUser = useSelector(SelectCurrentUserId)
   const owner = users.find((value) => value.id === pageId)
-  const allVideos = useSelector(SelectVideos)
   const loading = useSelector(SelectLoading)
-  const usersVideos = allVideos.filter((value) => value.userId === pageId)
-  let renderVideos
-  if (usersVideos.length > 0) {
-    renderVideos = usersVideos.map((element) => (
-      <Video data={element} key={element.id} />
-    ))
-  } else {
-    renderVideos = <h1>Nothing here.. yet</h1>
-  }
   return (
     <Layout>
       <div className="page-block">
@@ -49,14 +31,15 @@ export default function UserPage() {
         <div className="user-videos">
           <div className="user-videos__header">
             <div className="videos-text">
-              <h2>{owner.userName}s videos</h2>
-              <img src={vidIcon.src} alt={vidIcon.alt} />
+              <h2>{owner.userName}`s videos</h2>
+              <img src={VidIcon} alt="" />
             </div>
-            {/* eslint-disable-next-line react/jsx-no-bind */}
-            <Button text="Add video" click={showForm} />
+            {currentUser === pageId ? (
+              <Button text="Add video" click={openForm} />
+            ) : null}
           </div>
           <div className="videos">
-            {loading ? <LoadRing isBig /> : renderVideos}
+            {loading ? <LoadRing isBig /> : <Videos pageId={pageId} />}
           </div>
         </div>
       </div>
