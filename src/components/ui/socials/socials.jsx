684b71219738d3e107a610d3328fafa4efe41045
dsdyaz <dsdy06@gmail.com>
2023-02-02 01:40:55 +0200

    refactored socials component

--- src/components/ui/socials/socials.jsx
+++ src/components/ui/socials/socials.jsx
@@ -5,39 +5,27 @@ import TwitterIcon from "../../../assets/social-icons/twitter-icon.svg"
 import "./style.css"
 
 export default function Socials() {
-  const telegramIcon = {
-    src: TelegramIcon,
-    alt: "Telegram social logo",
-  }
-  const discordIcon = {
-    src: DiscordIcon,
-    alt: "Discord social logo",
-  }
-  const twitterIcon = {
-    src: TwitterIcon,
-    alt: "Twitter social logo",
-  }
   return (
     <div className="social-icons">
       <a href="https://web.telegram.org/z/">
         <img
           className="social-icons_icon"
-          src={telegramIcon.src}
-          alt={telegramIcon.alt}
+          src={TelegramIcon}
+          alt="our telegram"
         />
       </a>
       <a href="https://discord.com/">
         <img
           className="social-icons_icon"
-          src={discordIcon.src}
-          alt={discordIcon.alt}
+          src={DiscordIcon}
+          alt="our discord"
         />
       </a>
       <a href="https://twitter.com/?lang=uk">
         <img
           className="social-icons_icon"
-          src={twitterIcon.src}
-          alt={twitterIcon.alt}
+          src={TwitterIcon}
+          alt="our twitter"
         />
       </a>
     </div>
