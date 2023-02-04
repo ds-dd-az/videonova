79ae39cc9a5db22169086cbe55782bca5e8a95bb
dsdyaz <dsdy06@gmail.com>
2023-02-05 00:38:51 +0200

    added proptypes to layout component

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,11 +1,13 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import propTypes from "prop-types"
 import Header from "../ui/header/header"
 import Footer from "../ui/footer/footer"
 import Popup from "../ui/popup/popup"
 
-// eslint-disable-next-line react/prop-types
 export default function Layout({ children }) {
+  Layout.propTypes = {
+    children: propTypes.node.isRequired,
+  }
   return (
     <>
       <Popup />
