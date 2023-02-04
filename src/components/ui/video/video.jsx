358f0f03dd2651007ee0982eeb597b7a1332bd05
dsdyaz <dsdy06@gmail.com>
2023-02-05 00:30:37 +0200

    refactored video container

--- src/components/ui/video/video.jsx
+++ src/components/ui/video/video.jsx
@@ -1,39 +1,25 @@
-/* eslint-disable prefer-destructuring */
-/* eslint-disable react/prop-types */
 import React, { useState } from "react"
-import ReactPlayer from "react-player"
 import "./style.css"
-import Play from "./play-icon.png"
+import propTypes from "prop-types"
 import findPreview from "../../../external_func/find-preview-src/find-preview-src"
 import VideoPlayer from "./video-player"
+import Thumbnail from "./video-thumbnail"
 
 export default function Video({ data }) {
-  const url = data.url
+  Video.propTypes = {
+    data: propTypes.objectOf(propTypes.string).isRequired,
+  }
+  const { url } = data
   const [visibility, setVisibility] = useState(false)
   function toggleVisibility() {
     setVisibility(!visibility)
   }
   const imgSrc = findPreview(url)
   if (visibility) {
-    return <VideoPlayer url={url} func={() => toggleVisibility()} />
+    return (
+      <VideoPlayer data={data} func={() => toggleVisibility()} src={imgSrc} />
+    )
   }
 
-  return (
-    <div className="video-component">
-      <div
-        className="thumbnail-wrapper"
-        role="button"
-        tabIndex="0"
-        onKeyDown={toggleVisibility}
-        onClick={toggleVisibility}
-      >
-        <div className="play-icon">
-          <img src={Play} alt="" />
-        </div>
-        <img src={findPreview(url)} alt="" />
-      </div>
-      <h3>{data.title}</h3>
-      {data.description}
-    </div>
-  )
+  return <Thumbnail data={data} src={imgSrc} func={() => toggleVisibility()} />
 }
