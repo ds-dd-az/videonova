d7461c1840e5296b1968269cb22763ce6a79c5bd
dsdyaz <dsdy06@gmail.com>
2023-02-06 18:25:24 +0200

    working on header functionality

--- src/components/ui/header/header.jsx
+++ src/components/ui/header/header.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import "./style.css"
 import propTypes from "prop-types"
 import Logo from "../logo/logo"
@@ -7,7 +7,7 @@ import UserIcon from "../user-icon/user-icon"
 import Button from "../button/button"
 
 export default function Header(props) {
-  const { login } = props
+  const { login, user } = props
   const dispatch = useDispatch()
   function showForm() {
     dispatch({
@@ -23,34 +23,32 @@ export default function Header(props) {
      * variation of header based on authorisation
      */
     login: propTypes.bool,
+    /**
+     * info on current user
+     */
+    user: propTypes.objectOf(propTypes.string),
   }
   Header.defaultProps = {
     login: false,
+    user: null,
   }
 
-  // if (login) {
-  //   return (
-  //     <div className="header">
-  //       <Logo color="blue" />
-  //       <div
-  //         className="header__user-info"
-  //         role="button"
-  //         tabIndex={0}
-  //         onKeyDown={redirect}
-  //         onClick={redirect}
-  //       >
-  //         <UserIcon size="small" />
-  //         {username}
-  //       </div>
-  //     </div>
-  //   )
-  // }
+  if (login) {
+    return (
+      <div className="header">
+        <Logo color="blue" />
+        <div className="header__user-info" role="button" tabIndex={0}>
+          <UserIcon iconSrc={user.userPic} size="small" />
+          {user.userName}
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="header">
       <Logo color="blue" />
-      {/* eslint-disable-next-line react/jsx-no-bind */}
-      <Button text="Sign up" click={showForm} />
+      <Button text="Sign up" click={() => showForm()} />
     </div>
   )
 }
