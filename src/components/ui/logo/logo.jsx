bd6356eafe746c246be5ad2b58b545c3d588fc37
dsdyaz <dsdy06@gmail.com>
2023-02-07 23:53:31 +0200

    refactored logo component

--- src/components/ui/logo/logo.jsx
+++ src/components/ui/logo/logo.jsx
@@ -1,26 +1,21 @@
 import React from "react"
 import propTypes from "prop-types"
-import BlueLogo from "../../../assets/logos/logo-blue.png"
-import WhiteLogo from "../../../assets/logos/logo-white.png"
+import blueLogo from "../../../assets/logos/logo-blue.png"
+import whiteLogo from "../../../assets/logos/logo-white.png"
 
 export default function Logo(props) {
   const { color } = props
   Logo.propTypes = {
-    color: propTypes.string.isRequired,
-  }
-  if (color === "blue") {
-    return (
-      <div className="logo-blue">
-        <img src={BlueLogo} alt="videonova" />
-      </div>
-    )
+    /**
+     * color of the logo, may be "white" or "blue"
+     */
+    color: propTypes.oneOf(["white", "blue"]).isRequired,
   }
 
-  if (color === "white") {
-    return (
-      <div className="logo-white">
-        <img src={WhiteLogo} alt="videonova" />
-      </div>
-    )
-  }
+  const img = color === "blue" ? blueLogo : whiteLogo
+  return (
+    <div className={`logo-${color}`}>
+      <img src={img} alt="videonova" />
+    </div>
+  )
 }
