495dcb20f2bf5e9672d3479da905044bf4a89e1a
dsdy <dsdy06@gmail.com>
2023-02-20 18:15:09 +0200

    changed the way fonts are loading deleted app.css, also added a line-height reset to stop interface shaking on render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import "./styles/App.css"
 import "./styles/style.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { useDispatch } from "react-redux"
