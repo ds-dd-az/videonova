6601316967d63405171f85666082565a333a907d
dsdy <dsdy06@gmail.com>
2023-02-22 17:00:15 +0200

    changed prop description in button

--- src/components/ui/button/button.jsx
+++ src/components/ui/button/button.jsx
@@ -14,7 +14,7 @@ export default function Button(props) {
 
   Button.propTypes = {
     /**
-     *  Button classes
+     *  additional classNames
      */
     className: propTypes.string,
     /**
