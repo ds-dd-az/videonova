60bac4f4c2e5a3b2bdc09f12ee2247b2d001a8cf
dsdy <dsdy06@gmail.com>
2023-02-28 18:29:43 +0200

    configured usercard stories

--- src/components/ui/user-card/user-card.stories.jsx
+++ src/components/ui/user-card/user-card.stories.jsx
@@ -1,19 +1,24 @@
+import { number } from "prop-types"
 import React from "react"
 import UserCard from "./user-card"
 
 export default {
   title: "Components/ui/User-card",
   component: UserCard,
+  argTypes: {
+    vidAmount: number,
+    likeAmount: number,
+  },
 }
 
 function Template(args) {
   return (
-    <div style={{ backgroundColor: "#ccc" }}>
+    <div style={{ backgroundColor: "#ccc", width: "302px" }}>
       <UserCard {...args} />
     </div>
   )
 }
 
-export const Primary = Template.bind({})
+export const Card = Template.bind({})
 
-Primary.args = {}
+Card.args = { vidAmount: 100, likeAmount: 100 }
