46e0461cfb2883f78feb5bc1b44d4587b7fb10a2
dsdy <dsdy06@gmail.com>
2023-03-01 21:22:43 +0200

    fixed video thumbnail storybook

--- src/components/ui/video/video.stories.jsx
+++ src/components/ui/video/video.stories.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Thumbnail from "./video-thumbnail"
+import "../../../styles/video/video.css"
 
 export default {
   title: "Components/ui/video-thumbnail",
@@ -9,10 +10,19 @@ export default {
 
 function Template(args) {
   return (
-    <div style={{ backgroundColor: "#ccc" }}>
-      <Thumbnail {...args} />
+    <div style={{ backgroundColor: "white" }}>
+      <div className="video-component">
+        <Thumbnail {...args} />
+      </div>
     </div>
   )
 }
 
 export const VideoThumbnail = Template.bind({})
+
+VideoThumbnail.args = {
+  data: {
+    title: "Great video",
+    description: "This video is great indeed, must watch for everyone",
+  },
+}
