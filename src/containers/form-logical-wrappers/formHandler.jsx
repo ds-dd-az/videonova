5a1ddb9aa46760e2bbdfc9c8a350fad137e16c87
dsdy <dsdy06@gmail.com>
2023-03-03 13:05:15 +0200

    decoupled signUp logic

--- src/containers/form-logical-wrappers/formHandler.jsx
+++ src/containers/form-logical-wrappers/formHandler.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux"
 import { selectType } from "../../modules/form"
 import AddVideoWithLogic from "./add-new-video-logic"
 import SignInFormWithLogic from "./sign-in-logic"
+import SignUpFormWithLogic from "./sign-up-logic"
 
 export default function FormHandler() {
   const formType = useSelector(selectType)
@@ -11,8 +12,10 @@ export default function FormHandler() {
   if (formType === "addVideo") {
     return <AddVideoWithLogic />
   }
-
   if (formType === "signIn") {
     return <SignInFormWithLogic />
+  }
+  if (formType === "signUp") {
+    return <SignUpFormWithLogic />
   }
 }
