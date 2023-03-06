187ea219ea00d4fbe17c47514f1c2ea72aa11e22
dsdy <dsdy06@gmail.com>
2023-03-06 18:22:21 +0200

    added docs for error-message

--- src/components/ui/error-message/error-message.jsx
+++ src/components/ui/error-message/error-message.jsx
@@ -2,6 +2,11 @@ import React from "react"
 import "../../../styles/error-message/error-message.css"
 import propTypes from "prop-types"
 
+/**
+ * Error message for forms
+ * @param message - string, message that needs to be displayed
+ * @returns node, component with passed message
+ */
 export default function ErrorMessage(props) {
   const { message } = props
   ErrorMessage.propTypes = {
