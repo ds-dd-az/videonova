50a674b6e296c9b4b4c9d6fa70e0612d30f7f6ae
dsdy <dsdy06@gmail.com>
2023-03-06 17:27:24 +0200

    added docs for layout component

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,6 +4,12 @@ import Footer from "../ui/footer/footer"
 import Popup from "../ui/popup/popup"
 import AdaptiveHeader from "../../containers/header/header-container"
 
+/**
+ * layout component
+ * @param children - node, components wrapped by this layout component
+ * @returns node, children wrapped by header and hiden by default popup component which displays forms
+ */
+
 export default function Layout({ children }) {
   Layout.propTypes = {
     children: propTypes.node.isRequired,
