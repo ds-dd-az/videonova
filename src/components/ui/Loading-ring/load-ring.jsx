e8c7807d0d390b9dbd42a10ab6a20cd0437a876f
dsdy <dsdy06@gmail.com>
2023-03-11 21:13:53 +0200

    added docs for loading ring

--- src/components/ui/Loading-ring/load-ring.jsx
+++ src/components/ui/Loading-ring/load-ring.jsx
@@ -2,6 +2,11 @@ import React from "react"
 import propTypes from "prop-types"
 import "../../../styles/loading-ring/loading-ring.css"
 
+/**
+ * Loading spinner component
+ * @param isBig - bool, determines size of the ring, small for buttons, big for pages
+ * @returns node, spinning ring of requested size
+ */
 export default function LoadRing(props) {
   const { isBig } = props
   LoadRing.propTypes = {
