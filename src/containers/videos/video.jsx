ec2d3cd2e425eaba70a71f8120cc052153a8cb41
dsdyaz <dsdy06@gmail.com>
2023-03-16 15:31:32 +0200

    added docs for video logical container

--- src/containers/videos/video.jsx
+++ src/containers/videos/video.jsx
@@ -4,8 +4,16 @@ import findPreview from "../../external_func/find-preview-src/find-preview-src"
 import VideoPlayer from "../../components/ui/video/video-player"
 import Thumbnail from "../../components/ui/video/video-thumbnail"
 
+/**
+ * Video component logical wrapper
+ * @param data - object of strings, all the info about displayed video
+ * @returns node, thumbnail with visibility toggling function, after toggling visibility to true returns videoplayer with darkened background
+ */
 export default function Video({ data }) {
   Video.propTypes = {
+    /**
+     * all the info about displayed video
+     */
     data: propTypes.objectOf(propTypes.string).isRequired,
   }
   const { url } = data
