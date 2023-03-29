d1d9556242ad0cae60d030d2620c367a58759b5e
dsdy <dsdy06@gmail.com>
2023-03-29 18:14:19 +0300

    added docs for scrollToTop button

--- src/containers/scroll-to-top-button/scroll-button.jsx
+++ src/containers/scroll-to-top-button/scroll-button.jsx
@@ -2,6 +2,13 @@ import React, { useState } from "react"
 import Button from "../../components/ui/button/button"
 import scrollToTop from "../../external_func/scroll-to-the-top/scroll-to-the-top"
 
+/**
+ * Scroll To Top button
+ *
+ * becomes visible after document.documentElement.scrollTop value is more than 300,
+ * onClick scrolls to the top with behavior value "smooth"
+ * @returns node, Button with "go to the top" inner text
+ */
 export default function ScrollToTopButton() {
   const [visible, setVisible] = useState(false)
 
