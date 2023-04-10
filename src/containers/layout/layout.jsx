1030b92504d99f5c9b57466125e227cc306f5076
dsdy <dsdy06@gmail.com>
2023-04-10 18:00:39 +0300

    added lazy loader for footer

--- src/containers/layout/layout.jsx
+++ src/containers/layout/layout.jsx
@@ -1,7 +1,6 @@
 import React, { lazy } from "react"
 import propTypes from "prop-types"
 import { useSelector } from "react-redux"
-import Footer from "../../components/ui/footer/footer"
 import AdaptiveHeader from "../header/header-container"
 import {
   SelectUserLoading,
@@ -10,6 +9,7 @@ import {
 import LoadRing from "../../components/ui/Loading-ring/load-ring"
 
 const PopupWithLogic = lazy(() => import("../popup-logical-cont/popup-logic"))
+const Footer = lazy(() => import("../../components/ui/footer/footer"))
 /**
  * layout component
  * @param children - node, components wrapped by this layout component
