71e5e64398c001b524c6a3d84bfabaa746090295
dsdy <dsdy06@gmail.com>
2023-04-10 20:38:06 +0300

    deleted useEffect from signup form

--- src/containers/form-logical-wrappers/sign-up-logic.jsx
+++ src/containers/form-logical-wrappers/sign-up-logic.jsx
@@ -21,18 +21,13 @@ import FormSwitcher from "../../external_func/switch-form/form-switcher"
  */
 export default function SignUpFormWithLogic() {
   const dispatch = useDispatch()
-  let nameField
-  let passwordField
-  let confirmPassField
-  useEffect(() => {
-    nameField = document.getElementById("name")
-    passwordField = document.getElementById("password")
-    confirmPassField = document.getElementById("repeatPassword")
-  })
   const errorMessage = useSelector(SelectErrorMessage)
   const passwordError = useSelector(SelectPasswordError)
   const nameError = useSelector(SelectNameError)
   const register = () => {
+    const nameField = document.getElementById("name")
+    const passwordField = document.getElementById("password")
+    const confirmPassField = document.getElementById("repeatPassword")
     signUp(
       dispatch,
       nameField.value,
