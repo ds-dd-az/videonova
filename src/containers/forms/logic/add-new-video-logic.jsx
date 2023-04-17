c6930a818e4fff81c94263659e28eac88e8e85d8
dsdy <dsdy06@gmail.com>
2023-04-17 12:13:13 +0300

    implemented videocheck func

--- src/containers/forms/logic/add-new-video-logic.jsx
+++ src/containers/forms/logic/add-new-video-logic.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Form from "../forms/forms"
-import addNewVideo from "../../../external_func/add-video/add-video"
+import submitVideo from "../../../external_func/add-video/add-video"
 import VideoForm from "../forms/add-new-video/add-new-video"
 import { SelectErrorMessage } from "../../../modules/current_error"
 import CloseFormButton from "../../close-form-button/close-form-button"
@@ -16,16 +16,16 @@ import Button from "../../../components/ui/button/button"
 export default function AddVideoWithLogic() {
   const dispatch = useDispatch()
   const errorMessage = useSelector(SelectErrorMessage)
-  const addVideo = () => {
+  const submit = () => {
     const linkField = document.getElementById("videoLink")
     const nameField = document.getElementById("videoName")
     const descField = document.getElementById("videoDesc")
-    addNewVideo(dispatch, linkField.value, nameField.value, descField.value)
+    submitVideo(dispatch, linkField.value, nameField.value, descField.value)
   }
   return (
     <Form>
       <VideoForm
-        submitFunc={addVideo}
+        submitFunc={submit}
         errorMessage={errorMessage}
         formCloser={
           <CloseFormButton>
